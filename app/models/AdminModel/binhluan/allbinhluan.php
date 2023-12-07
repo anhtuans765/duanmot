dc0db49d9924f47f8464c906191e3186495e2860
anhtuan <tuannsaph38615@fpt.edu.vn>
2023-12-07 22:12:54 +0700

    fix lỗi all bình luận

--- app/models/AdminModel/binhluan/allbinhluan.php
+++ app/models/AdminModel/binhluan/allbinhluan.php
@@ -10,6 +10,7 @@
     <table class="table">
     <thead>
         <tr>
+
             <th scope="col">Mã Bình Luận</th>
             <th scope="col">Tên Tài Khoản Bình Luận</th>
             <th scope="col">Mã Khóa Học Được Bình Luận</th>
@@ -17,24 +18,34 @@
             <th scope="col">Ngày Bình Luận</th>
             <th scope="col">Đánh Giá</th>
             <th scope="col">Thao tác</th>
+
         </tr>
     </thead>
+
     <tbody>
+
         <?php
+
         $binhluan = select_all_table();
         foreach($binhluan as $row):
         extract($row); ?>
         <tr>
+
             <th scope="row"><?=$id_binh_luan ?></th>
             <td><?=$id_tai_khoan ?></td>
             <td><?=$id_khoa_hoc ?></td>
             <td><?=$noi_dung_binh_luan ?></td>       
             <td><?=$ngay_binh_luan ?></td>       
             <td><?=$danh_gia ?></td>       
-            <td><a href="index.php?act=delete&header=allbinhluan&id=id_binh_luan&table=binh_luan&iddl=<?=$id_binh_luan;?>"><button class="btn btn-danger">Xóa</button></a></td>       
+            <td><a href="index.php?act=delete&header=allbinhluan&id=id_binh_luan&table=binh_luan&iddl=<?=$id_binh_luan;?>"><button class="btn btn-danger">Xóa</button></a></td>
+                   
         </tr>
+
         <?php endforeach; ?>
+
     </tbody>
     </table>
+
+
 </body>
 </html>
