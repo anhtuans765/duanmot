0376ca2b8244167602cbd09f4500e4cd1f14b95e
anhtuan <tuannsaph38615@fpt.edu.vn>
2023-12-11 13:23:59 +0700

    sua all binh luan

--- app/models/AdminModel/binhluan/allbinhluan.php
+++ app/models/AdminModel/binhluan/allbinhluan.php
@@ -18,34 +18,24 @@
             <th scope="col">Ngày Bình Luận</th>
             <th scope="col">Đánh Giá</th>
             <th scope="col">Thao tác</th>
-
         </tr>
     </thead>
-
     <tbody>
-
         <?php
-
         $binhluan = select_all_table();
         foreach($binhluan as $row):
         extract($row); ?>
         <tr>
-
             <th scope="row"><?=$id_binh_luan ?></th>
             <td><?=$id_tai_khoan ?></td>
             <td><?=$id_khoa_hoc ?></td>
             <td><?=$noi_dung_binh_luan ?></td>       
             <td><?=$ngay_binh_luan ?></td>       
             <td><?=$danh_gia ?></td>       
-            <td><a href="index.php?act=delete&header=allbinhluan&id=id_binh_luan&table=binh_luan&iddl=<?=$id_binh_luan;?>"><button class="btn btn-danger">Xóa</button></a></td>
-                   
+            <td><a href="index.php?act=delete&header=allbinhluan&id=id_binh_luan&table=binh_luan&iddl=<?=$id_binh_luan;?>"><button class="btn btn-danger">Xóa</button></a></td>          
         </tr>
-
         <?php endforeach; ?>
-
     </tbody>
     </table>
-
-
 </body>
 </html>
